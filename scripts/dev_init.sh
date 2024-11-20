659a23da4fa2992ee57c60127fcd00968fe7a06d
captaincoordinates <thomaschristian@gmail.com>
2024-11-20 16:41:57 +0000

    added pre-commit config

--- scripts/dev_init.sh
+++ scripts/dev_init.sh
@@ -4,4 +4,10 @@ set -e
 
 pushd $(dirname $0)/..
 
-pip install -r src/generate/requirements.txt
+# These deps to support a local IDE in syntax highlighting etc., all execution happens in Docker containers
+pip install -r src/requirements_common.txt
+pip install -r src/requirements_113.txt
+
+# Enforce pre-commit hook
+pip install pre-commit
+pre-commit install
